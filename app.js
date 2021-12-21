8a147ff2287e28095d7fe2ac1afb479919435cc3
George Ciurca <george.ciurca@gmail.com>
2021-12-21 14:26:12 +0000

    errors updated

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-const remainingCharsElement = document.getElementById('remaining-chars');
-const productNameInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById("remaining-chars");
+const productNameInputElement = document.getElementById("product-name");
 
 const maxAllowedChars = productNameInputElement.maxLength;
 
-function updateRemainingCharacters (event) {
-    const enteredText = event.target.value;
-    const enteredTextLength = enteredText.length;
+function updateRemainingCharacters(event) {
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-    const remainingCharacters = maxAllowedChars - enteredTextLength;
- 
-    remainingCharsElement.textContent = remainingCharacters;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
-    if (remainingCharacters <=10) {
-        remainingCharsElement.classList.add('warning');
-        productNameInputElement.classList.add('warning');
-    }
+  remainingCharsElement.textContent = remainingCharacters;
+
+  if (remainingCharacters === 0) {
+    remainingCharsElement.classList.add("error");
+    productNameInputElement.classList.add("error");
+  } else if (remainingCharacters <= 10) {
+    remainingCharsElement.classList.add("warning");
+    productNameInputElement.classList.add("warning");
+  } else {
+    remainingCharsElement.classList.remove("warning");
+    productNameInputElement.classList.remove("warning");
+    remainingCharsElement.classList.remove("error");
+    productNameInputElement.classList.remove("error");
+  }
 }
 
-productNameInputElement.addEventListener('input', updateRemainingCharacters);
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
 
-const spanElement = document.getElementById('remaining-chars');
+const spanElement = document.getElementById("remaining-chars");
 /*spanElement.style.color = 'red'; */
 
 /* spanElement.classList.add('warning'); */
-
